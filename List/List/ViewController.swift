3c66b4f59f0da3e2ab613ebc158e007e25ac3017
Rob <rob@Robs-MacBook-Pro.local>
2019-05-08 22:23:38 -0400

    Intro to xcode final project

---
+++ List/List/ViewController.swift
@@ -0,0 +1,65 @@
+//
+//  ViewController.swift
+//  List
+//
+//  Created by Rob on 5/8/19.
+//  Copyright © 2019 Rob. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate
+{
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    let textCellIdentifier = "item"
+    var data: [String] = []
+    var count = 0
+    
+    let swiftBlogs = ["Ray Wenderlich", "NSHipster", "iOS Developer Tips", "Jameson Quave", "Natasha The Robot", "Coding Explorer", "That Thing In Swift", "Andrew Bancroft", "iAchieved.it", "Airspeed Velocity"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        for i in 0...1000 {
+//            data.append("\(i)")
+//        }
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    // MARK:  UITextFieldDelegate Methods
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(textCellIdentifier, forIndexPath: indexPath)
+        
+        let row = indexPath.row
+        cell.textLabel?.text = data[row] //swiftBlogs[row]
+        
+        return cell
+    }
+    
+    // MARK:  UITableViewDelegate Methods
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        
+        let row = indexPath.row
+        print(data[row])//swiftBlogs[row])
+    }
+    
+    @IBAction func btnAdd(sender: AnyObject) {
+        data.append("\(count)")
+        count+=1
+        tableView.reloadData()
+        
+    }
+    
+}
+
---
+++ advanced_xcode/Filterer_C3_Starting/FilterWatch Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  FilterWatch Extension
+//
+//  Created by Rob on 4/29/19.
+//  Copyright © 2019 UofT. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
